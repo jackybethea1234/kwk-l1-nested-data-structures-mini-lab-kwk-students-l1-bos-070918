b8d38653c47e9f69c82f92772f883b312630d93f
jackybethea1234 <jackybethea@gmail.com>
2018-07-11 19:53:12 +0000

    automatically backed up by learn

---
+++ hash_intro.rb
@@ -0,0 +1,19 @@
+#intialize empty hash
+items = {}
+
+items["Dazzling pair of shoes"]=110
+
+puts items
+
+
+new_item = "Crazy Socks"
+new_price = 10 
+
+items[new_item]=new_price
+
+puts items
+
+
+while 
+  puts "omfg im dead"
+  
